a03fe9c9d1ac1b6f01a48ba59495267cd8e25107
magaNasib <mehemmednessibov3@gmail.com>
2023-11-14 19:34:54 +0400

    first commit

--- src/components/ChangableContent.js
+++ src/components/ChangableContent.js
@@ -9,14 +9,14 @@ export function ChangableContent({ heading, arr, save, isCity, isCountry }) {
     return (
         <>
             <h3 className='text-center text-[#141D24] font-bold text-4xl my-10'>{heading}</h3>
-            <div className='grid md:grid-cols-3 lg:grid-cols-3 sm:grid-cols-2 justify-center gap-6 flex-wrap'>
+            <div className='grid grid-cols-3 justify-center gap-6 flex-wrap'>
                 {
                     arr.map(({ id, img, title, trip, date, price }) => {
                         return (
                             <div key={id} className='rounded-2xl border border-[#E3E7EB] text-[#141D24]'>
                                 <Link to={navigate}>
                                     <div className='relative'>
-                                        <img src={img} alt={title} />
+                                        <img src={img} className='w-full h-full object-cover' alt={title} />
                                         {save && <button className='p-3 rounded-full bg-white text-[#4A21EF] absolute right-4 top-4'><BsBookmark className='text-[1.1rem]' /></button>}
                                     </div>
                                     <div className='p-4'>
